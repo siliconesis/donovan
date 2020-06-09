879d927c4d70621e691fc9db5660197d4f45f4dd
Russell McDaniel <russell.mcdaniel@siliconesis.com>
2020-06-08 22:22:31 -0700

    Resolve CA1052: Static holder types should be Static or NotInheritable

--- src/Donovan.Game.Web.Api/Program.cs
+++ src/Donovan.Game.Web.Api/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace Donovan.Game.Web.Api
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
--- src/Donovan.Game.Web/Program.cs
+++ src/Donovan.Game.Web/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Logging;
 
 namespace Donovan.Game.Web
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
